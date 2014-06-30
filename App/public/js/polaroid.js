b6b9a9a15264869f31cacc86d4b76623d5565059
redliontom <tomschalbar@hotmail.com>
2014-06-30 09:51:45 +0200

    Navigation, News Feed

---
+++ App/public/js/polaroid.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+    
+    /* Toogle menu right */
+    $('.toggle').click(function() {
+        $('#toggle_menu').slideToggle( 400 );
+        return false;
+    });
+    
+    $('.content').click(function() {
+        $('#toggle_menu').hide( 400 );
+    });
+    
+    
+    /* Dynamisches nachladen */
+    /* News Feed */
+    $('#link_feed').click(function() {
+        $('.content').load('../acc/files/feed.html', function(){
+            $('[id^=write_comment_]').click(function() {
+                $var=$(this).attr('id').match(/[0-9]+/)[0];
+                $show='.nc_'+$var;
+                $($show).slideToggle( 400 );
+                return false;
+            });
+            
+            $('[id^=show_comment_]').click(function(){
+                $var=$(this).attr('id').match(/[0-9]+/)[0];
+                $show='.cs_'+$var;
+                $($show).slideToggle(400);
+                return false;
+            });
+        });
+    });
+    /* Default */
+    if ($(".content").html() === ""){
+        $('.content').load('../acc/files/feed.html', function(){
+            $('[id^=write_comment_]').click(function() {
+                $var=$(this).attr('id').match(/[0-9]+/)[0];
+                $show='.nc_'+$var;
+                $($show).slideToggle(400);
+                return false;
+            });
+            
+            $('[id^=show_comment_]').click(function(){
+                $var=$(this).attr('id').match(/[0-9]+/)[0];
+                $show='.cs_'+$var;
+                $($show).slideToggle(400);
+                return false;
+            });
+        });
+    };
+    
+});
+
